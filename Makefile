bba9f17620e1654efc3ac1ff5db12183999ff651
Mimsy <3504550573@qq.com>
2024-10-30 19:51:22 +0800

    使用Cmake编译文件

--- Makefile
+++
@@ -1,5 +0,0 @@
-CC = gcc
-
-vorpal: vorpal.c
-	$(CC) vorpal.c -o vorpal -Wall -Wextra -pedantic -std=c99
-
---
+++ build/CMakeFiles/Veitor.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Veitor.dir/src/vorpal.c.o"
+  "CMakeFiles/Veitor.dir/src/vorpal.c.o.d"
+  "bin/Veitor"
+  "bin/Veitor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Veitor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
